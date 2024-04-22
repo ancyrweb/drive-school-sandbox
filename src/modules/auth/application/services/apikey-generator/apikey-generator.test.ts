4ac8cb459f0b36dac54e189a3b2ec1353ee9f32e
Anthony Cyrille <ancyr@pm.me>
2024-04-22 05:18:28 +0200

    fix: errorz

--- src/modules/auth/application/services/apikey-generator/apikey-generator.test.ts
+++ src/modules/auth/application/services/apikey-generator/apikey-generator.test.ts
@@ -1,9 +1,7 @@
 import { ApikeyGenerator } from './apikey-generator.js';
-import { InstructorId } from '../../../domain/entities/instructor-id.js';
 import { RamUserRepository } from '../../../infrastructure/persistence/ram/ram-user-repository.js';
-import { UserId } from '../../../domain/entities/user-id.js';
-import { User } from '../../../domain/entities/user.js';
 import { Apikey } from '../../../domain/entities/apikey.js';
+import { UserBuilder } from '../../../tests/factories/user-builder.js';
 
 class TestableRandomApiKeyGenerator extends ApikeyGenerator {
   private availableKeys = ['key1', 'key2'];
@@ -24,13 +22,9 @@ test('when the apikey is available, it should use the first one', async () => {
 
 test('when the apikey is not available, should pick another one', async () => {
   const userRepository = new RamUserRepository([
-    User.create({
-      id: new UserId(),
-      accountId: new InstructorId(),
-      emailAddress: 'johndoe@gmail.com',
-      password: 'azerty123',
+    new UserBuilder({
       apikey: Apikey.generate('key1'),
-    }),
+    }).build(),
   ]);
 
   const generator = new TestableRandomApiKeyGenerator(userRepository);
